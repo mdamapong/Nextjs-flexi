c281678de35460438424abf1381cae7add71cc55
mickey <damapongm@gmail.com>
2023-07-01 10:17:50 -0700

    footer work

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,52 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { footerLinks } from '@/constants';
+
+type ColumnProps = {
+	title: string;
+	links: Array<string>;
+};
+
+const FooterColumn = ({ title, links }: ColumnProps) => (
+	<div className='footer_column'>
+		<h4 className='font-semibold'>{title}</h4>
+		<ul className='flex flex-col gap-2 font-normal'>
+			{links.map((link) => (
+				<Link href='/' key={link}>
+					{link}
+				</Link>
+			))}
+		</ul>
+	</div>
+);
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<footer className='flexStart footer'>
+			<div className='flex flex-col gap-12 w-full'>
+				{' '}
+				<div className='flex items-start flex-col'>
+					<Image
+						src='/logo-purple.svg'
+						width={115}
+						height={38}
+						alt='Flexibble'
+					/>
+					<p className='text-start text-sm font-normal mt-5 max-w-xs'>
+						Flexibble is the the world's leading community for creatives to
+						share, grow, and get hired.
+					</p>
+				</div>
+				<div className='flex flex-wrap gap-12'>
+					<FooterColumn
+						title={footerLinks[0].title}
+						links={footerLinks[0].links}
+					/>
+				</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
