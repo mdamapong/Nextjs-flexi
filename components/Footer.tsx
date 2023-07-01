df85bb04d3e46b89cfe3f9e5d26ac713d4dcc1d9
mickey <damapongm@gmail.com>
2023-07-01 10:56:57 -0700

    footer done

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -43,7 +43,43 @@ const Footer = () => {
 						title={footerLinks[0].title}
 						links={footerLinks[0].links}
 					/>
+					<div className='flex-1 flex flex-col gap-4'>
+						<FooterColumn
+							title={footerLinks[1].title}
+							links={footerLinks[1].links}
+						/>
+						<FooterColumn
+							title={footerLinks[2].title}
+							links={footerLinks[2].links}
+						/>
+					</div>
+					<FooterColumn
+						title={footerLinks[3].title}
+						links={footerLinks[3].links}
+					/>
+					<div className='flex-1 flex flex-col gap-4'>
+						<FooterColumn
+							title={footerLinks[4].title}
+							links={footerLinks[4].links}
+						/>
+						<FooterColumn
+							title={footerLinks[5].title}
+							links={footerLinks[5].links}
+						/>
+					</div>
+					<FooterColumn
+						title={footerLinks[6].title}
+						links={footerLinks[6].links}
+					/>
 				</div>
+			</div>
+
+			<div className='flexBetween footer_copyright'>
+				<p>@ 2023 Flexibble. All rights reserved</p>
+				<p className='text-gray'>
+					<span className='text-black font-semibold'>10,214</span> projects
+					submitted
+				</p>
 			</div>
 		</footer>
 	);
